e43437e54c7b1b8bc25e521c749a8df339f8a87b
HananeBENARAB <101798507+HananeBENARAB@users.noreply.github.com>
2022-05-20 14:08:16 +0200

    Add files via upload

---
+++ Personne.java
@@ -0,0 +1,22 @@
+package Cours2;
+
+public class Personne {
+	private String nom ;
+
+	public Personne() {
+	}
+	
+	public Personne(String nom) {
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
